9b9342465058c6c5539ad25c16201f2e7381b7c6
Bhagyesh-20 <bhagyesh.dh20@gmail.com>
2024-07-21 23:18:13 +0530

    Changes the database

--- frontend/src/utils/useAxios.js
+++ frontend/src/utils/useAxios.js
@@ -4,7 +4,7 @@ import dayjs from "dayjs";
 import { useContext } from "react";
 import AuthContext from "../context/AuthContext";
 
-const baseURL = "http://127.0.0.1:8000/prediction";
+const baseURL = "http://13.127.141.157:8000/prediction/";
 
 const useAxios = () => {
   const { authTokens, setUser, setAuthTokens } = useContext(AuthContext);
