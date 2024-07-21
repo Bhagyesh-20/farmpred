107f18c7e1fbeff98ef53b91ea667ed829c6dd10
Bhagyesh-20 <bhagyesh.dh20@gmail.com>
2024-07-21 23:27:59 +0530

    ADded IP

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-t%9rwdkqmreqze1xcux@z9_i!%cfa2_9zlhscue30wyj&plyys
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['13.127.141.157','localhost']
+ALLOWED_HOSTS = ['43.205.31.243','localhost']
 
 
 # Application definition
