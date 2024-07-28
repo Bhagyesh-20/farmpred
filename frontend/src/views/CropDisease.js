5612341bcd521c5cd20174a8f3a8078deb62a8db
Bhagyesh-20 <bhagyesh.dh20@gmail.com>
2024-07-28 19:21:38 +0530

    modified:   frontend/src/views/CropDisease.js

--- frontend/src/views/CropDisease.js
+++ frontend/src/views/CropDisease.js
@@ -4,7 +4,7 @@ import AuthContext from '../context/AuthContext';
 import './CropDisease.css';
 import remedies from './Remedies.json';
 
-const baseURL = 'https://api-ml-1.onrender.com/';
+const baseURL = 'https://api-ml-1.onrender.com';
 
 const CropDisease = () => {
     const { authTokens } = useContext(AuthContext);
