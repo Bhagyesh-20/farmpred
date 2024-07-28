ac2dc61d8c3dd58e160f9d11170d5ae7a7419b4c
Bhagyesh-20 <bhagyesh.dh20@gmail.com>
2024-07-28 21:06:55 +0530

    modified:   prediction/views.py

--- backend/prediction/views.py
+++ backend/prediction/views.py
@@ -7,14 +7,14 @@ from rest_framework.response import Response
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework import generics, status, viewsets
 from rest_framework.permissions import AllowAny, IsAuthenticated
-from rest_framework.parsers import MultiPartParser, FormParser
-from rest_framework.views import APIView
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras.models import load_model 
+# from rest_framework.parsers import MultiPartParser, FormParser
+# from rest_framework.views import APIView
+# import tensorflow as tf
+# from tensorflow import keras
+# from tensorflow.keras.models import load_model 
 
-from PIL import Image
-import numpy as np
+# from PIL import Image
+# import numpy as np
 import logging
 import os
 
@@ -53,42 +53,42 @@ def testEndPoint(request):
 
 logger = logging.getLogger(__name__)
 
-class PredictView(APIView):
-    parser_classes = (MultiPartParser, FormParser)
+# class PredictView(APIView):
+#     parser_classes = (MultiPartParser, FormParser)
 
-    def post(self, request, *args, **kwargs):
-        file = request.data.get('file')
-        if not file:
-            return Response({"error": "No file provided"}, status=status.HTTP_400_BAD_REQUEST)
+#     def post(self, request, *args, **kwargs):
+#         file = request.data.get('file')
+#         if not file:
+#             return Response({"error": "No file provided"}, status=status.HTTP_400_BAD_REQUEST)
 
-        file = request.data['file']
-        image = Image.open(file)
+#         file = request.data['file']
+#         image = Image.open(file)
         
-        # Preprocess the image to fit your model input requirements
-        image = image.resize((224, 224))  # Example for a 224x224 input size
-        image = np.array(image) / 255.0  # Normalize to [0,1] range
-        image = np.expand_dims(image, axis=0)  # Add batch dimension
+#         # Preprocess the image to fit your model input requirements
+#         image = image.resize((224, 224))  # Example for a 224x224 input size
+#         image = np.array(image) / 255.0  # Normalize to [0,1] range
+#         image = np.expand_dims(image, axis=0)  # Add batch dimension
 
-        # Load your model (ensure this is loaded only once in production)
-        model = load_model(model_path)
+#         # Load your model (ensure this is loaded only once in production)
+#         model = load_model(model_path)
         
-        # Make prediction
-        prediction = model.predict(image)
+#         # Make prediction
+#         prediction = model.predict(image)
         
-        # Assuming the prediction is a class label
-        predicted_class = np.argmax(prediction)
-        class_names = ['Anthracnose', 'Apple Scab', 'Black Spot', 
-                       'Blight', 'Blossom End Rot','Botrytis','Brown Rot',
-                       'Canker','Cedar Apple Rust','Clubroot','Crown Gall',
-                       'Downy Mildew','Fire Blight','Fusarium','Gray Mold',
-                       'Leaf Spots','Mosaic Virus','Nematodes','Powdery Mildew',
-                       'Verticilium']
+#         # Assuming the prediction is a class label
+#         predicted_class = np.argmax(prediction)
+#         class_names = ['Anthracnose', 'Apple Scab', 'Black Spot', 
+#                        'Blight', 'Blossom End Rot','Botrytis','Brown Rot',
+#                        'Canker','Cedar Apple Rust','Clubroot','Crown Gall',
+#                        'Downy Mildew','Fire Blight','Fusarium','Gray Mold',
+#                        'Leaf Spots','Mosaic Virus','Nematodes','Powdery Mildew',
+#                        'Verticilium']
         
-        predicted_class_name = class_names[predicted_class]
+#         predicted_class_name = class_names[predicted_class]
 
-        logger.debug("Received file: %s", file.name)
+#         logger.debug("Received file: %s", file.name)
         
-        return Response({"prediction": predicted_class_name}, status=status.HTTP_200_OK)
+#         return Response({"prediction": predicted_class_name}, status=status.HTTP_200_OK)
     
 
 class ProductViewSet(viewsets.ModelViewSet):
