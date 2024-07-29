da938d13b3698726f7667616df57a2c9992ba825
Bhagyesh-20 <bhagyesh.dh20@gmail.com>
2024-07-29 12:14:11 +0530

    deployment files

--- backend/build_files.sh
+++ backend/build_files.sh
@@ -1,4 +1,6 @@
+#!/bin/bash
+
 echo "BUILD START"
-python 3.9 - m pip install -r requirements.txt requirements.txt
-python 3.9 manage.py collectstatic --noinput --clear
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput --clear
 echo "BUILD END"
